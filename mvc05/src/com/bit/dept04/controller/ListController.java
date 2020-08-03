c2e13028053ed6031f3d9d03eb0e5639f6f43632
bit01class <bit01class@gmail.com>
2020-08-03 11:27:55 +0900

    tomcat connection pools

--- mvc05/src/com/bit/dept04/controller/ListController.java
+++ mvc05/src/com/bit/dept04/controller/ListController.java
@@ -16,6 +16,7 @@ public class ListController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		
 		Dept04Dao dao=null;
 		dao=new Dept04Dao();
 		req.setAttribute("list", dao.selectAll());
--- mvc05/src/com/bit/dept04/model/Dept04Dao.java
+++ mvc05/src/com/bit/dept04/model/Dept04Dao.java
@@ -1,17 +1,96 @@
 package com.bit.dept04.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class Dept04Dao {
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public Dept04Dao() {
+		// connection pools api
+		//http://tomcat.apache.org/tomcat-7.0-doc/jdbc-pool.html
+		// context.xml 다음추가
+		/*
+		<Resource 
+			auth="Container" 
+			name="jdbc/oracle" 
+			type="javax.sql.DataSource" 
+			driverClassName="oracle.jdbc.OracleDriver" 
+			url="jdbc:oracle:thin:@localhost:1521:xe" 
+			username="scott"
+			password="tiger" 
+			maxActive="50" 
+			maxIdle="100" 
+			maxWait="10000" 
+			minIdle="10" 
+		/>
+		*/
+		try {
+			InitialContext init = new InitialContext();
+			javax.naming.Context context = null;
+			context=(Context) init.lookup("java:/comp/env");
+			System.out.println(context);
+			javax.sql.DataSource dataSource=null;
+			dataSource=(DataSource) context.lookup("jdbc/oracle");
+			
+			
+			System.out.println(dataSource);
+			conn=dataSource.getConnection();
+			System.out.println(conn);
+		} catch (NamingException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		//////////////////////////////////////////////
+//		try {
+//			Class.forName("oracle.jdbc.OracleDriver");
+//			conn=DriverManager.getConnection(
+//					"jdbc:oracle:thin:@localhost:1521:xe",
+//					"scott","tiger");
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//		}
+	}
 
 	public List<Dept04Dto> selectAll(){
 		List<Dept04Dto> list=new ArrayList<Dept04Dto>();
-		for(int i=0; i<10; i++){
-		list.add(new Dept04Dto(
-				1000*(i+1),
-				"테스트"+i,"테스트지역"
-				));
+		try{
+			pstmt=conn.prepareStatement("select * from dept04");
+			rs=pstmt.executeQuery();
+			while(rs.next()){
+			list.add(new Dept04Dto(
+					rs.getInt("deptno"),
+					rs.getString("dname"),
+					rs.getString("loc")
+					));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				if(rs!=null)rs.close();
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 		return list;
 	}
